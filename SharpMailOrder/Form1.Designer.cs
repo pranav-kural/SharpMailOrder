16bb3b26c85d8ea4d9e7b989557327c7dd175639
Pranav Kural <pranavkural042@gmail.com>
2017-01-22 20:55:34 -0500

    Added the calculate, print and clear buttons

--- SharpMailOrder/Form1.Designer.cs
+++ SharpMailOrder/Form1.Designer.cs
@@ -43,6 +43,9 @@
             this.hoursWorked = new System.Windows.Forms.TextBox();
             this.totalSales = new System.Windows.Forms.TextBox();
             this.salesBonus = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -189,11 +192,44 @@
             this.salesBonus.TabIndex = 11;
             this.salesBonus.TextChanged += new System.EventHandler(this.salesBonus_TextChanged);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F);
+            this.button3.Location = new System.Drawing.Point(277, 320);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 35);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F);
+            this.button2.Location = new System.Drawing.Point(152, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Print";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F);
+            this.button1.Location = new System.Drawing.Point(16, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(368, 367);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.salesBonus);
             this.Controls.Add(this.totalSales);
             this.Controls.Add(this.hoursWorked);
@@ -233,6 +269,9 @@
         private System.Windows.Forms.TextBox hoursWorked;
         private System.Windows.Forms.TextBox totalSales;
         private System.Windows.Forms.TextBox salesBonus;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
