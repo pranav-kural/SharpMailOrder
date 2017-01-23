04e8f297d95941ef15d7e8b781870c7857baf5b0
Pranav Kural <pranavkural042@gmail.com>
2017-01-22 20:52:09 -0500

    Added the languages GroupBox

--- SharpMailOrder/Form1.Designer.cs
+++ SharpMailOrder/Form1.Designer.cs
@@ -28,20 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.Français = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Français);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(165, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 89);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Languages";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F);
+            this.radioButton1.Location = new System.Drawing.Point(16, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "English";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Français
+            // 
+            this.Français.AutoSize = true;
+            this.Français.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F);
+            this.Français.Location = new System.Drawing.Point(16, 53);
+            this.Français.Name = "Français";
+            this.Français.Size = new System.Drawing.Size(83, 22);
+            this.Français.TabIndex = 1;
+            this.Français.Text = "Français";
+            this.Français.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(368, 344);
+            this.ClientSize = new System.Drawing.Size(368, 367);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Français;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
