bfa2c3e064158e118dd6d4c2311bb52a7c4fccd0
Pranav Kural <pranavkural042@gmail.com>
2017-01-27 17:54:50 -0500

    Added the function to calculate the sales bonus

--- SharpMailOrder/MailOrder.cs
+++ SharpMailOrder/MailOrder.cs
@@ -24,8 +24,11 @@ namespace SharpMailOrder
 
             switch (MailOrderButton.Tag.ToString())
             {
-                // clear all the text boxes
+                case "calculateButton":
+                    salesBonusTextBox.Text = CalculateSalesBonus().ToString();
+                    break;
                 case "clearButton":
+                    // clear all the text boxes
                     employeeNameTextBox.Text = "";
                     employeeIDTextBox.Text = "";
                     hoursWorkedTextBox.Text = "";
@@ -44,6 +47,27 @@ namespace SharpMailOrder
         
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private double CalculateSalesBonus()
+        {
+            // parsing the values of total hours worked and total sales
+            int totalHoursWorked = 0;
+            double totalSales = 0;
+            if (Int32.TryParse(hoursWorkedTextBox.Text, out totalHoursWorked) && Double.TryParse(totalSalesTextBox.Text, out totalSales))
+            {
+                totalHoursWorked = Int32.Parse(hoursWorkedTextBox.Text);
+                totalSales = Double.Parse(totalSalesTextBox.Text);
+
+                // calculating the sales bonus
+                return (((double)totalHoursWorked / 160) * (totalSales * 0.02));
+            }
+            else
+            {
+                MessageBox.Show("Error: Invalid value for the total hours worked  or total sales field");
+                return 0;
+            }
 
         }
 
