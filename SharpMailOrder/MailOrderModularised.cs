9efcca0db4986fa4866d90408a9fd1baebff317e
Pranav Kural <pranavkural042@gmail.com>
2017-01-29 14:12:46 -0500

    Added the comment headers to all the C# files

--- SharpMailOrder/MailOrderModularised.cs
+++ SharpMailOrder/MailOrderModularised.cs
@@ -1,4 +1,27 @@
-﻿
+﻿///<summary>
+/// Georgian College - Computer Programmer
+/// COMP 1004 - Rapid Application Development
+/// Instructor: Tom Tsiliopoulos
+/// 
+/// Assignment 1: Sharp Mail Order – SALES BONUS
+/// Description: The Company has instituted a bonus program to give its employees an incentive to sell more.	
+/// For every dollar the store makes in	a four-week period, the employees receive 2	percent of sales.The
+/// amount of bonus each employee receives is based upon the percentage of hours he or she worked
+/// during the bonus period(a total of  160	hours)
+/// 
+/// Author Name: Pranav Kural
+/// Student Number: 200333253
+/// 
+/// Last modified: January 29, 2017
+/// 
+/// Brief revision history:
+/// Initial commit to add default .gitIgnore and .gitAttribute files.
+/// .....
+/// Improved the logic for validating the hours worked by giving up use of RegEx
+/// Added the logic to validate that total sales is a an integer or a decimal number
+/// Gave all Windows Form and all attached UI Controls appropriate Variable names
+/// Added logic to represent total sales as currency value and added commenting
+/// </summary>
 /*
 using System;
 using System.Collections.Generic;
